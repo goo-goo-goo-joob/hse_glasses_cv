a80d4f4b4bd58e9e0d1daa93433f6c21f8dc9214
Андрей Солодянкин <andrsolo21@gmail.com>
2022-12-15 17:45:13 +0300

    add crop class

---
+++ cropp_image.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import numpy as np
+from google.colab.patches import cv2_imshow
+import matplotlib.pyplot as plt
+
+
+class YUNET_CROP(object):
+    def __init__(self, face_detection_model, score_threshold = 0.9, 
+                 nms_threshold = 0.3, top_k = 100, size = (320, 320), 
+                 scale = 1, padding = (0,0), draw_images = False, thickness  =2):
+        self.scale = scale
+        self.w_padding = padding[0]
+        self.h_padding = padding[1]
+        self.draw_images = draw_images
+        self.thickness = 2
+        self.detector = cv.FaceDetectorYN.create(
+            face_detection_model,
+            "",
+            size,
+            score_threshold,
+            nms_threshold,
+            top_k
+        )
+    def crop(self, img1):
+        img1Width = int(img1.shape[1]*args["scale"])
+        img1Height = int(img1.shape[0]*args['scale'])
+        img1 = cv.resize(img1, (img1Width, img1Height))
+        self.detector.setInputSize((img1Width, img1Height))
+        faces = self.detector.detect(img1)
+        imgs = []
+        for i in faces[1]:
+            coords = i.astype(np.int32)
+            nk = (int(coords[0] - self.h_padding ),int(coords[1] - self.w_padding))
+            wh = (int(coords[0]+  self.h_padding + coords[2]), int(coords[1]+self.w_padding + coords[3]))
+
+            imgs.append(img1[nk[1]:wh[1], nk[0]:wh[0]])
+            if self.draw_images:
+                copy_img = img1.copy()
+                cv.rectangle(copy_img, nk, wh, (0, 255, 0), self.thickness)
+        if self.draw_images:
+            cv2_imshow(img1)
+        return faces[1], imgs
